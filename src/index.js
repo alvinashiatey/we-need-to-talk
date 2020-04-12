f44be574b295058ab2ac3926a1721379d2ba589f
alvinashiatey <alvinashiatey@gmail.com>
2020-04-11 23:38:26 -0400

    transcription added

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ const apiCall = async () => {
   const channel = "bryant-wells-eeaqnoam1yc";
   const makeURL = (page, per) =>
     `http://api.are.na/v2/channels/${channel}?page=${page}&amp;per=${per}`;
-  //
+
   axios
     .get(makeURL(1, 1))
     .then((res) => {
-      console.log(res);
+      const transcription = document.getElementById("transcription");
+      const dataTrs = res.data.contents[0].content_html;
+      console.log(dataTrs);
+      transcription.innerHTML = dataTrs;
     })
     .catch((err) => {
       console.log(err);
