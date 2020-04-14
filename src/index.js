6587fe5878635dbcf598b314f833e723b99a01f6
alvinashiatey <alvinashiatey@gmail.com>
2020-04-13 23:40:16 -0400

    https

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const transcription = document.getElementById("transcription");
 const apiCall = async () => {
   const channel = "bryant-wells-eeaqnoam1yc";
   const makeURL = (page, per) =>
-    `http://api.are.na/v2/channels/${channel}?page=${page}&amp;per=${per}`;
+    `https://api.are.na/v2/channels/${channel}?page=${page}&amp;per=${per}`;
 
   const data = axios
     .get(makeURL(1, 1))
