e4dcfc7a5c02fe88b38ed2e9b87f998d394e8626
alvinashiatey <alvinashiatey@gmail.com>
2020-04-18 11:04:07 -0400

    stagger turned off

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import "./styles/reset/reset.scss";
 import "./styles/main.scss";
-import { revealBtn } from "./js/animation";
+import {
+  revealBtn,
+  revealAboutUs,
+  hideAboutUs,
+  revealparagraph,
+} from "./js/animation";
 import axios from "axios";
 import runtime from "serviceworker-webpack-plugin/lib/runtime";
 
 const musicDiv = document.querySelector(".music__btn");
 const transcription = document.getElementById("transcription");
+const interviewP = document.querySelectorAll("#transcription p");
 
 const addTargetLink = (str) => {
   const regSpaceBtnA = /((<a)\s(?=href))/gi;
@@ -13,7 +19,7 @@ const addTargetLink = (str) => {
   return pArr;
 };
 
-const classnameTag = (str2, loc2) => {
+const classnameTag = (str2, loc2, p) => {
   const regB = /(<)(p)(>)(Bryant:)/gi;
   let sArr = str2.replace(
     regB,
@@ -32,13 +38,13 @@ const apiCall = async () => {
     .then((res) => {
       const dataTrs = res.data.contents[0].content_html;
       const interviewReq = document.querySelector(".interview__req");
-
       interviewReq.addEventListener("click", (e) => {
         e.preventDefault();
 
         //Regex funtion on strings
         let text = addTargetLink(dataTrs);
-        classnameTag(text, transcription);
+        classnameTag(text, transcription, interviewP);
+        //revealparagraph(transcription.children, 0.1);
 
         if ("caches" in window) {
           caches.open("interviews").then((cache) => {
@@ -97,12 +103,31 @@ musicPlayer();
 //Animation calls
 const animationCalls = () => {
   const interviewReq = document.querySelector(".interview__req");
+  let aboutUs = document.querySelector(".about-us__info");
+  const aboutUsBtn = document.querySelector(".about-us__btn");
   let state = true;
+  let r = true;
+
   interviewReq.addEventListener("click", (e) => {
     if (state) {
       state = !state;
       revealBtn(musicDiv, 0.6);
     }
+  });
+  aboutUsBtn.addEventListener("click", () => {
+    if (r) {
+      r = !r;
+      revealAboutUs(aboutUs, 1);
+    } else {
+      r = !r;
+      hideAboutUs(aboutUs, 1);
+    }
+  });
+
+  const str = apiCall();
+  str.then((res) => {
+    const regA = /<p>[\w].+?\/p>/gi;
+    const pTags = res.match(regA);
   });
 };
 
--- src/js/animation.js
+++ src/js/animation.js
@@ -11,4 +11,29 @@ let revealBtn = (div, dur) => {
   });
 };
 
-export { revealBtn };
+let revealAboutUs = (div1, dur1) => {
+  gsap.set(div1, { visibility: "visible" });
+  gsap.to(div1, dur1, {
+    x: 40,
+    ease: "power3.inOut",
+  });
+};
+
+let hideAboutUs = (div1, dur1) => {
+  gsap.to(div1, dur1, {
+    x: -300,
+    ease: "power3.inOut",
+  });
+  gsap.set(div1, { visibility: "hidden", delay: dur1 });
+};
+
+let revealparagraph = (str, dur2) => {
+  gsap.from(str, dur2, {
+    y: 13,
+    opacity: 0,
+    ease: "power3.in",
+    stagger: 0.1,
+  });
+};
+
+export { revealBtn, revealAboutUs, hideAboutUs, revealparagraph };
